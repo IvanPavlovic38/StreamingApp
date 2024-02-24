07be890d75ca72f5b11c2e88568d267ed53e6ac2
IvanPavlovic38 <149716519+IvanPavlovic38@users.noreply.github.com>
2024-02-24 09:23:19 +0100

    Add files via upload

---
+++ Izbornik.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.StreamingApp
+{
+    internal class Izbornik
+    {
+        private void PozdravnaPoruka()
+        {
+            Console.WriteLine("***********************************");
+            Console.WriteLine("********** Streaming app **********");
+            Console.WriteLine("***********************************");
+        }
+
+        public Izbornik() 
+        {
+            Pomocno.dev = false;
+            ObradaTrazi = new ObradaTrazi();
+            ObradaZanrovi = new ObradaZanrovi();
+            ObradaVasaLista = new ObradaVasaLista();
+            ObradaPovijest = new ObradaPovijest();
+            PozdravnaPoruka();
+            PrikaziIzbornik();
+        }
+
+        private void PrikaziIzbornik()
+        {
+            Console.WriteLine("Glavni izbornik");
+            Console.WriteLine("1. Traži serije");
+            Console.WriteLine("2. Žanrovi");
+            Console.WriteLine("3. Vaša lista");
+            Console.WriteLine("4. Povijest gledanja");
+            Console.WriteLine("5. Izlaz iz programa");
+
+            switch ()
+            {
+                case 1:
+                    ObradaTrazi.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    ObradaZanrovi.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    ObradaVasaLista.PrikaziIzbornik();
+                    PrikaziIzbornik(); ;
+                    break;
+                case 4:
+                    ObradaPovijest.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                case 5:
+                    Console.WriteLine("Hvala na korištenju, doviđenja");
+                    break;
+            }
+        }
+    }
+}
---
+++ ObradaTrazi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UcenjeCS.StreamingApp.Model;
+
+namespace UcenjeCS.StreamingApp
+{
+    internal class ObradaTrazi
+    {
+        public List<Serija> Serije { get; }
+
+        public ObradaTrazi()
+        {
+            Serije = new List<Serija>();
+            if (Pomocno.dev)
+            {
+                TestniPodaci();
+            }
+        }
+
+        public void PrikaziIzbornik()
+        {
+            Console.WriteLine("Tražilica");
+            Console.WriteLine("1. Traži po nazivu");
+            Console.WriteLine("2. Traži po žanru");
+            Console.WriteLine("3. Traži po datumu izdavanja");
+
+            switch ()
+            {
+                case 1:
+                    TraziNaziv();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    TraziZanr();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    TraziDatum();
+                    PrikaziIzbornik();
+                    break;
+            }
+
+            private void TraziNaziv() { }
+
+            private void TraziZanr() { }
+
+            private void TraziDatum() { }
+        }
+    }
+}
