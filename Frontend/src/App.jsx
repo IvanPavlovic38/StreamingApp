aa28c309a24fbe58a8540c67c5e731bbf94d3bd2
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-03-21 16:18:10 +0100

    dodao app i navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import Pocetna from "./pages/Pocetna"
+import { RoutesNames } from "./constants"
+import NavBar from "./components/NavBar"
+import Epizode from "./pages/epizode/Epizode"
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import EpizodeDodaj from "./pages/Epizode/EpizodeDodaj"
+import EpizodePromjeni from "./pages/epizode/EpizodePromjeni"
+
+import Serije from "./pages/serije/Serije"
+import SerijeDodaj from "./pages/serije/SerijeDodaj"
+import SerijePromjeni from "./pages/serije/SerijePromjeni"
+
+
+import Korisnici from "./pages/korisnici/Korisnici"
+import KorisniciDodaj from "./pages/korisnici/KorisniciDodaj"
+import KorisniciPromjeni from "./pages/korisnici/KorisniciPromjeni"
+import Zanrovi from "./pages/zanrovi/Zanrovi"
+import ZanroviDodaj from "./pages/zanrovi/ZanroviDodaj"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Routes>
+        <>
+          <Route path={RoutesNames.HOME} element={<Pocetna />} />
+
+          
+          <Route path={RoutesNames.EPIZODE_PREGLED} element={<Epizode />} />
+          <Route path={RoutesNames.EPIZODE_NOVI} element={<EpizodeDodaj />} />
+          <Route path={RoutesNames.EPIZODE_PROMJENI} element={<EpizodePromjeni />} />
+
+
+          <Route path={RoutesNames.SERIJE_PREGLED} element={<Serije />} />
+          <Route path={RoutesNames.SERIJE_NOVI} element={<SerijeDodaj />} />
+          <Route path={RoutesNames.SERIJE_PROMJENI} element={<SerijePromjeni />} />
+
+          <Route path={RoutesNames.KORISNICI_PREGLED} element={<Korisnici />} />
+          <Route path={RoutesNames.KORISNICI_NOVI} element={<KorisniciDodaj />} />
+          <Route path={RoutesNames.KORISNICI_PROMJENI} element={<KorisniciPromjeni />} />
+
+          <Route path={RoutesNames.ZANROVI_PREGLED} element={<Zanrovi />} />
+          <Route path={RoutesNames.ZANROVI_NOVI} element={<ZanroviDodaj />} />
+          <Route path={RoutesNames.ZANROVI_PROMJENI} element={<ZanroviPromjeni />} />
+
+
+          <Route path={RoutesNames.SEZONE_PREGLED} element={<Sezone />} />
+          <Route path={RoutesNames.SEZONE_NOVI} element={<SezoneDodaj />} />
+          <Route path={RoutesNames.SEZONE_PROMJENI} element={<SezonePromjeni />} />
+        </>
+      </Routes>
     </>
   )
 }
---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,66 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from 'react-router-dom';
+import { RoutesNames } from '../constants';
+
+import './NavBar.css';
+
+function NavBar() {
+
+    const navigate = useNavigate();
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand 
+            className='linkPocetna'
+            onClick={()=>navigate(RoutesNames.HOME)}
+        >
+            Streaming app
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            
+            <NavDropdown title="Programi" id="basic-nav-dropdown">
+              <NavDropdown.Item 
+                onClick={()=>navigate(RoutesNames.EPIZODE_PREGLED)}
+              >
+                Epizode
+              </NavDropdown.Item>
+              <NavDropdown.Item 
+              onClick={()=>navigate(RoutesNames.SERIJE_PREGLED)}
+              >
+                Serije
+              </NavDropdown.Item>
+              <NavDropdown.Item 
+              onClick={()=>navigate(RoutesNames.KORISNICI_PREGLED)}
+              >
+                Korisnici
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item 
+              onClick={()=>navigate(RoutesNames.ZANROVI_PREGLED)}
+              >
+                Zanrovi
+              </NavDropdown.Item>
+              <NavDropdown.Item 
+                onClick={()=>navigate(RoutesNames.SEZONE_PREGLED)}
+              >
+                Sezone
+              </NavDropdown.Item>
+            </NavDropdown>
+            
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+        <Nav.Link target="_blank" href="https://ivanpavlovic-001-site1.ltempurl.com/swagger/index.html">API dokumentacija</Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
